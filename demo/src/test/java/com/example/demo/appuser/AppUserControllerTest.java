83afacec4f470f1a3ac85ffe2181d9e0ad53eb28
NadaFouad214 <48562476+NadaFouad214@users.noreply.github.com>
2022-12-11 16:23:17 +0200

    initial commit

--- demo/src/test/java/com/example/demo/appuser/AppUserControllerTest.java
+++ demo/src/test/java/com/example/demo/appuser/AppUserControllerTest.java
@@ -1,67 +1,86 @@
 package com.example.demo.appuser;
 
+import com.example.demo.games.DiceScoreGame;
+import com.example.demo.games.PokemonDamageCalculatorGame;
+import com.example.demo.games.RockPaperScissorsGame;
 import com.example.demo.games.TowerOfHanoiGame;
-import org.junit.Before;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@AutoConfigureMockMvc
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@ExtendWith(MockitoExtension.class)
 class AppUserControllerTest {
 
-    @Autowired
-    private MockMvc mvc;
     @InjectMocks
     private AppUserController appUserController;
 
-    @Before
-    public void setUp() throws Exception {
-        mvc = MockMvcBuilders.standaloneSetup(appUserController).build();
-    }
+    @Mock
+    private  AppUserService appUserService;
+
+
     @Test
-    void game() throws Exception {
-
-//        RequestBuilder builder = MockMvcRequestBuilders.post("/discs");
-//        MvcResult result=mvc.perform(builder).andReturn();
-//        TowerOfHanoiGame towerOfHanoiGame = new TowerOfHanoiGame();
-//        towerOfHanoiGame.setMove(3);
-//        assertEquals(towerOfHanoiGame,result.getResponse().getContentAsString());
-//
-//       mvc.perform(MockMvcRequestBuilders
-//               .post("/discs").contentType(MediaType.APPLICATION_JSON))
-//               .andExpect(status().isOk())
-//               .andExpect(MockMvcResultMatchers.jsonPath()
-
-
-
-
-
-
-
+    void TowerGame() throws Exception {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+        TowerOfHanoiGame towerOfHanoiGame = new TowerOfHanoiGame(3);
+        appUserService.TowerOfHanoi(towerOfHanoiGame);
+        ResponseEntity<Object> responseEntity = appUserController.game(towerOfHanoiGame);
+        assertThat(responseEntity.getStatusCodeValue()).isEqualTo(201);
+        assertThat(responseEntity.getHeaders().getLocation().getPath()).isEqualTo("/discs");
     }
 
     @Test
-    void testGame() {
+    void RSPTestGame() throws Exception {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+        RockPaperScissorsGame rockPaperScissorsGame=new RockPaperScissorsGame();
+        rockPaperScissorsGame.setPlayer1("Rock");
+        rockPaperScissorsGame.setPlayer2("Paper");
+        appUserService.RockPaperScissors(rockPaperScissorsGame);
+        ResponseEntity<Object> responseEntity = appUserController.game(rockPaperScissorsGame);
+        assertThat(responseEntity.getStatusCodeValue()).isEqualTo(201);
+        assertThat(responseEntity.getHeaders().getLocation().getPath()).isEqualTo("/rsp");
+
+
     }
 
     @Test
-    void testGame1() {
+    void PCDTestGame1() throws Exception {
+
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+       PokemonDamageCalculatorGame pokemonDamageCalculatorGame=new PokemonDamageCalculatorGame();
+        pokemonDamageCalculatorGame.setPlayer1("fire");
+        pokemonDamageCalculatorGame.setPlayer2("water");
+        pokemonDamageCalculatorGame.setDefense(100);
+        pokemonDamageCalculatorGame.setAttack(100);
+        appUserService.PokemonDamageCalc(pokemonDamageCalculatorGame);
+        ResponseEntity<Object> responseEntity = appUserController.game(pokemonDamageCalculatorGame);
+        assertThat(responseEntity.getStatusCodeValue()).isEqualTo(201);
+        assertThat(responseEntity.getHeaders().getLocation().getPath()).isEqualTo("/pcd");
+
     }
 
     @Test
-    void testGame2() {
+    void DiceScoreTestGame2() throws Exception {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+        DiceScoreGame diceScoreGame=new DiceScoreGame();
+        diceScoreGame.setArr(new int[]{4,4,4,3,3});
+        appUserService.DiceScore(diceScoreGame);
+        ResponseEntity<Object> responseEntity = appUserController.game(diceScoreGame);
+        assertThat(responseEntity.getStatusCodeValue()).isEqualTo(201);
+        assertThat(responseEntity.getHeaders().getLocation().getPath()).isEqualTo("/dic");
+
     }
 }
